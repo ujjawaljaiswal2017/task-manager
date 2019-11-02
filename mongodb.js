ac25d6bfcc9f7e105bf04a39154a59b05e88d301
ujjawaljaiswal2017 <ujjawaljaiswal2017@gmail.com>
2019-11-02 19:50:55 +0530

    Added conection to database

--- mongodb.js
+++ mongodb.js
@@ -1,13 +1,17 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
-const connectionURL= ''
-const database = 'task-manager'
+const connectionURL= 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL , {useNewUrlParser : true}, (error, client)=>{
+MongoClient.connect(connectionURL , {useNewUrlParser : true , useUnifiedTopology:true}, (error, client)=>{
     if(error){
         return console.log('Error occured')
     }    
 
-    console.log('Connection succesful')
+    const db = client.db(databaseName)
+    db.collection('users').insertOne({
+        name : 'Ujjawal',
+        age : 20
+    })
 })
