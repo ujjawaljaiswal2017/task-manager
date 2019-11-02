826f89638d3e5771a6f773e172c51d0abfaabbb3
ujjawaljaiswal2017 <ujjawaljaiswal2017@gmail.com>
2019-11-02 19:39:48 +0530

    Added conection to database

--- mongodb.js
+++ mongodb.js
@@ -3,3 +3,11 @@ const MongoClient = mongodb.MongoClient
 
 const connectionURL= ''
 const database = 'task-manager'
+
+MongoClient.connect(connectionURL , {useNewUrlParser : true}, (error, client)=>{
+    if(error){
+        return console.log('Error occured')
+    }    
+
+    console.log('Connection succesful')
+})
