18245d6a5d8504ca5770f36734ae3bbde7e7a3ca
ujjawaljaiswal2017 <ujjawaljaiswal2017@gmail.com>
2019-11-09 12:23:12 +0530

    Removed unecessary code

--- mongodb.js
+++ mongodb.js
@@ -6,6 +6,10 @@ const {MongoClient, ObjectID } = require('mongodb')
 const connectionURL= 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
+
 MongoClient.connect(connectionURL , {useNewUrlParser : true , useUnifiedTopology:true}, (error, client)=>{
     if(error){
         return console.log('Error occured')
